4c21c3d53092dd8f184664e7e82deb687972809d
DavoGG <davidjgg198@gmail.com>
2021-08-11 23:16:56 -0400

    server plays once board provided

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, request, Response
+import json
+import random
 
 app = Flask('name')
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    print(request.json)
+    return  jsonify(serverplay(request.json))
 
-def goodbye_world():
-    return "<p>Goodbye, World!</p>"
+# dale pues maldita puta codea
+def produceBoard():
+    board = [[None, None, None],
+             [None, None, None],
+             [None, None, None]]
+    return board
+
+def jsonify(input):
+    return Response(
+        json.dumps(input),
+        mimetype="application/json"
+    )
+def serverplay(board):
+    line = random.randint(0,2)
+    column = random.randint(0,2)
+    
+    board[line][column] = 0 
+    return board
